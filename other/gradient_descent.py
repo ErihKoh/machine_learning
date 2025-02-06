dc47ab203d15c3d4186fb1b043e4572f31bb9a74
Yurii Zubar <63535943+ErihKoh@users.noreply.github.com>
2025-02-06 18:39:58 +0200

    add gradient descent sin

--- other/gradient_descent.py
+++ other/gradient_descent.py
@@ -3,19 +3,27 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
+# def f(x):
+#     return x * x - 5 * x + 5
+#
+#
+# def df(x):
+#     return 2*x - 5  # похідна
+
 def f(x):
-    return x * x - 5 * x + 5
+    return np.sin(x) + 0.5*x
 
 
 def df(x):
-    return 2*x - 5  # похідна
+    return np.cos(x) + 0.5  # похідна
 
 
 N = 20  # кількість ітерацій
 xx = 0  # початкове значення
 lmd = 0.1  # крок
 
-x_plt = np.arange(0, 5.0, 0.1)
+# x_plt = np.arange(0, 5.0, 0.1)
+x_plt = np.arange(-5.0, 5.0, 0.1)
 f_plt = [f(x) for x in x_plt]
 
 plt.ion()  # інтерактивний режим відображення графіка
@@ -26,8 +34,10 @@ ax.plot(x_plt, f_plt)  # парабола
 point = ax.scatter(xx, f(xx), c='red')  # точка червоним кольором
 
 #  початока роботи алгоритму градієртного спуску
+mn = 100
 for i in range(N):
-    xx = xx - lmd*df(xx)  # зміна аргументу на поточній ітерації
+    lmd = 1 / min(i + 1, mn)
+    xx = xx - lmd*np.sign(df(xx))  # зміна аргументу на поточній ітерації
 
     point.set_offsets([xx, f(xx)])  # відображення нового положення точки
 
